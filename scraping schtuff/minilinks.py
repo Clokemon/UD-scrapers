12a57b74deff7c29ecbe50bd79f88720e31d85dc
clokemon <dexter.marc.rose@gmail.com>
2025-06-09 13:13:55 -0700

    ooo look its the scrapers

---
+++ scraping schtuff/minilinks.py
@@ -0,0 +1,34 @@
+import requests  # type: ignore
+from bs4 import BeautifulSoup  # type: ignore
+import os
+import random
+alphabetAgain = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet = list(alphabetAgain)
+
+t = 1
+
+for i in alphabet:
+    if os.path.exists("mini_scraped_links" + i + ".txt"):
+        alphabet.pop(alphabetAgain.find(i))
+        alphabetAgain = "".join(alphabet)
+        print("File mini_scraped_links" + i + ".txt already exists, skipping...")
+
+for i in alphabet:
+    f = open("mini_scraped_links" + i +".txt", "x")
+    with open("mini_scraped_links" + i +".txt", "a") as f:
+        t = 0
+        f.write("https://www.urbandictionary.com/browse.php?character=" + i + "\n")
+        while True:
+            t += 1
+            r = requests.get("https://www.urbandictionary.com/browse.php?character=" + i +"&page=" + str(t))
+            soup = BeautifulSoup(r.content, "html.parser")
+            s = soup.find("ul", class_="mt-3 columns-2 md:columns-3")
+            content = soup.find_all('a', class_="py-1 block text-denim dark:text-white break-all hover:text-limon-lime hover:underline")
+            if not content:
+                break
+            for j in content:
+                f.write("https://www.urbandictionary.com" + j['href'] + "&page=" + str(1) + "\n")
+                if (random.randint(0, 1) == 0):
+                    print("https://www.urbandictionary.com" + j['href'] + "&page=" + str(1) + "\n")
+                
+print("done")
